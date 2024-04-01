44ac3417c1c1143ed03f1e976d8d8ed8656e7732
isaacR23 <163067528+isaacR23@users.noreply.github.com>
2024-04-01 16:09:24 -0700

    Endpoint excersice

--- GoServ.go
+++ GoServ.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits++
+        next.ServeHTTP(w, r)
+    })
+}
 
 
 
@@ -17,23 +29,46 @@ func main() {
 		Addr:    ":" + port,
 		Handler: corsMux,
 	}
-	// Serve the assets directory under the path /app/assets/
-	logoDir := http.FileServer(http.Dir("./assets/"))
-	mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", logoDir))
-	// Serve index.html at the path /app
-	mux.HandleFunc("/app", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./index.html")
+
+	var apiCfg apiConfig
+	handler := http.FileServer(http.Dir("./assets/"))
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(handler))
+
+	mux.Handle("/app", apiCfg.middlewareMetricsInc(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome to Chirpy"))
+	})))
+
+
+	mux.HandleFunc("GET /metrics",func(curl http.ResponseWriter, req *http.Request){
+		// note, you get information from the client aka the response when you send something
+		// with the req. 
+		curl.WriteHeader(http.StatusOK)
+		var hits string = fmt.Sprintf("Hits: %v", apiCfg.fileserverHits)
+		curl.Write([]byte(hits))
 	})
 
-	mux.HandleFunc("/healthz",func(curl http.ResponseWriter, req *http.Request){
+	mux.HandleFunc("/reset",func(curl http.ResponseWriter, req *http.Request){
+		// note, you get information from the client aka the response when you send something
+		// with the req. 
+		apiCfg.fileserverHits = 0
+		curl.WriteHeader(http.StatusOK)
+	})
+
+	mux.HandleFunc("GET /healthz",func(curl http.ResponseWriter, req *http.Request){
 
 		curl.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		curl.WriteHeader(http.StatusOK)
 		curl.Write([]byte("OK"))
 	})
 
+	// Serve the assets directory under the path /app/assets/
+	logoDir := http.FileServer(http.Dir("./assets/"))
+	mux.Handle("/app/assets/", http.StripPrefix("/app/assets/", logoDir))
+
 	log.Printf("Serving on port: http://localhost:%s\n", port)
-	log.Fatal(srv.ListenAndServe())
+	log.Fatal(srv.ListenAndServe()) // This exe the serv. Anything after this line is never ran
 }
 
 func middlewareCors(next http.Handler) http.Handler {
