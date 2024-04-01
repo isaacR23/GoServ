136eb19631121d6a18ea2c40859616fdc41f38e1
isaacR23 <163067528+isaacR23@users.noreply.github.com>
2024-04-01 08:41:19 -0700

    Second ex, serve html

--- GoServ.go
+++ GoServ.go
@@ -1,38 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
-type apiHandler struct{}
 
-func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
 
 func main() {
+	const port = "8080"
+
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
 
-	mux.Handle("/api/", apiHandler{})
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-
-		if req.URL.Path != "/" {
-			http.NotFound(w, req)
-			return
-		}
-		fmt.Fprintf(w, "Welcome to the home page!")
-	})
-
-	server := &http.Server{
-		Addr:    ":8080",
+	srv := &http.Server{
+		Addr:    ":" + port,
 		Handler: corsMux,
 	}
 
-	// fmt.Println("Server is running on http://localhost:8080")
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("Error starting server:", err)
-	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
 
 func middlewareCors(next http.Handler) http.Handler {
@@ -41,7 +28,7 @@ func middlewareCors(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusNotFound) // change for http.StatusOK for code 200 OK
+			w.WriteHeader(http.StatusOK)
 			return
 		}
 		next.ServeHTTP(w, r)
